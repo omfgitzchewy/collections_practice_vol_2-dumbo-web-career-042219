a3f833f1562644ea9f488cc320b551182ab3ba9c
Aaron Chu <aaronchu357@gmail.com>
2019-04-10 01:32:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,8 +59,8 @@ end
 
 def organize_schools(schools)
   final = Hash.new
-  schools.map do |school, hash|
-    hash.map do |location, city|
+  schools.each do |school, hash|
+    hash.each do |location, city|
       if final.key?(city) == false
         final[city] = [school]
       else
